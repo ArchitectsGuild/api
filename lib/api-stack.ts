cf55d45b9a5a79880418a9365f83d7bf8118c570
intiamaru <jorgeboneu@gmail.com>
2024-10-18 10:02:00 -0500

    first changes

--- lib/api-stack.ts
+++ lib/api-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Crear la Lambda utilizando Node.js 20
+    const helloLambda = new lambda.Function(this, 'HelloWorldLambda', {
+      runtime: lambda.Runtime.NODEJS_20_X, // Node.js 20
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          return { statusCode: 200, body: 'Hello World' };
+        };
+      `),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Crear una API Gateway que exponga la Lambda
+    new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
+      handler: helloLambda,
+    });
   }
 }
